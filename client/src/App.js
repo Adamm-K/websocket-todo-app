9b01b542ffb430fcc2b0e072084d1ff64169c408
Adam Kwidzyński <adamkwidzynski@gmail.com>
2022-11-07 23:31:46 +0100

    Task 28.4

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import shortid from 'shortid';
+
+const App = () => {
+  const [socket, setSocket] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
+  useEffect(() => {
+    const socket = io('http://localhost:8000');
+    setSocket(socket);
+    socket.on('updateData', (tasks) => {
+      updateTasks(tasks);
+    });
+    socket.on('removeTask', (id) => {
+      removeTask(id);
+    });
+    socket.on('addTask', (task) => {
+      addTask(task);
+    });
+  }, []);
+
+  const updateTasks = (tasksData) => {
+    setTasks(tasksData);
+  };
+
+  const removeTask = (taskId, isLocal) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+    if (isLocal) {
+      socket.emit('removeTask', taskId);
+    }
+  };
+
+  const addTask = (task) => {
+    setTasks((tasks) => [...tasks, task]);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    addTask({ name: taskName, id: shortid() });
+    socket.emit('addTask', { name: taskName, id: shortid() });
+  };
+
+  return (
+    <div className='App'>
+      <header>
+        <h1>ToDoList.app</h1>
+      </header>
+
+      <section className='tasks-section' id='tasks-section'>
+        <h2>Tasks</h2>
+
+        <ul className='tasks-section__list' id='tasks-list'>
+          {tasks.map((task) => (
+            <li key={task.id} className='task'>
+              {task.name}{' '}
+              <button
+                className='btn btn--red'
+                onClick={() => removeTask(task.id, true)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <form id='add-task-form' onSubmit={(e) => submitForm(e)}>
+          <input
+            className='text-input'
+            autoComplete='off'
+            type='text'
+            placeholder='Type your description'
+            id='task-name'
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+          <button className='btn' type='submit'>
+            Add
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+let tasks = [];
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', (taskId) => {
+    tasks = tasks.filter((task) => task.id !== taskId);
+    socket.broadcast.emit('removeTask', taskId);
+  });
+});
